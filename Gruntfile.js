214618572aa012abea822d07e06a54b6ff22c750
Jelle Versele <jelle.versele@internetarchitects.be>
2015-05-19 13:03:55 +0200

    Added dev and test tasks to gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,26 @@ module.exports = function (grunt) {
 	'use strict';
 
 	grunt.initConfig({
+
 		jest: {
 			options: {
 				coverage: true,
 				testPathPatern: /.*.spec.js/
 			}
+		},
+
+		watch: {
+			tests: {
+				files: ['lib/**/*.js', '__mocks__/**/*.js', '__tests__/**/*.js'],
+				tasks: ['jest']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-jest');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('dev', ['jest', 'watch']);
+	grunt.registerTask('test', ['jest']);
 
 };
